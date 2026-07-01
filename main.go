1e1c49ad540adc4c654c6a40d3e8b491986eb983
Asad <aq8829@outlook.com>
2026-07-01 19:01:33 -0400

    initial commit chirpy project

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./")))
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	srv.ListenAndServe()
+
+}
