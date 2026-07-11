a9d11f2070383ed996aad2048dd008cb54870b6e
Asad <aq8829@outlook.com>
2026-07-11 19:01:34 -0400

    added healthz, metrics, and reset handlers / registered their mux routes

--- main.go
+++ main.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	FileserverHits atomic.Int32
+}
+
 func main() {
+	cfg := &apiConfig{}
+	fs := http.FileServer(http.Dir("./"))
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
+	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", cfg.numberOfRequests)
+	mux.HandleFunc("/reset", cfg.resetHitCount)
 
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	srv.ListenAndServe()
+	log.Fatal(srv.ListenAndServe())
+}
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.FileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) numberOfRequests(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.FileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d\n", hits)
+}
+
+func (cfg *apiConfig) resetHitCount(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	cfg.FileserverHits.Store(0)
+	w.Write([]byte("Hit count reset\n"))
 }
